d92132bd922db4a7a3f8ae038101283ac5af93c8
AdnanM50 <a-19150@mangrove.edu.bd>
2025-04-07 23:08:44 +0600

    Refactor variable and array declarations to improve clarity and organization

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-let a = 1;
-var b = 2 
-const c = 3;
+
+// how to declear a variable and prient them 
+
+// let a = 1;
+// var b = 2 
+// const c = 3;
 
 
-console.log("a = ", a)
-console.log("b = ", b)
-console.log("c = ", c)
+// console.log("a = ", a)
+// console.log("b = ", b)
+// console.log("c = ", c)
+//  b = 5
+// console.log("b = ", b)
+
+
+// how to declear an array in js 
+
+
+// let array = [1, 3, 4, 6, 8, 7, 9]
+
+
+// console.log("🚀 ~ array:", array)
+
+// how to write if else condition in js 
+
+
+// if (condition) {
+//     console.log('')
+// } else {
+//     console.log('')
+// }
+
+
+// how to write for loop condition in js
+
+
+// for (let  i= 0; i < 7; i++) {
+// }
+
+
+
+
+
+let adnan = [1, 3, 4, 6, 8, 7, 9]
+// console.log("adnan ar size", adnan.length)
+for (let i = 0; i < adnan.length; i++) {
+    
+    const element = adnan[i];
+    console.log("🚀 ~ element: kno ashtise", element)
+
+    if (element % 2 == 0 ) {
+        console.log( element ,"this is an even number")
+        
+    } else {
+        console.log(element ,"this is an odd number")
+    }
+}
